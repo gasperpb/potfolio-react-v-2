8ee7bb3822614adf7d155cf6e0e5371d20e00b3a
gasperpb <achillespb@hotmail.com>
2024-02-18 11:05:24 -0300

    organizando as qualificacoes

--- src/components/Qualification/Qualification.js
+++ src/components/Qualification/Qualification.js
@@ -1,114 +1,116 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { SectionQualificationStyle } from './Qualification.style';
+import { SectionQualificationStyle } from "./Qualification.style";
 
 const Qualification = () => {
   const [toggle, setToggle] = useState(1);
 
   return (
-    <SectionQualificationStyle className='section'>
-      <h2 className='section-title'>Qualification</h2>
-      <span className='section-subtitle'>My personal journey</span>
+    <SectionQualificationStyle className="section">
+      <h2 className="section-title">Qualification</h2>
+      <span className="section-subtitle">My personal journey</span>
 
-      <div className='qualification-container container'>
-        <div className='qualification-tabs'>
+      <div className="qualification-container container">
+        <div className="qualification-tabs">
           <div
-            className='qualification-button'
-            style={{ color: `${toggle === 1 ? 'var(--first-color)' : ''}` }}
+            className="qualification-button"
+            style={{ color: `${toggle === 1 ? "var(--first-color)" : ""}` }}
             onClick={() => setToggle(1)}
           >
-            <i className='uil uil-graduation-cap qualification-icon'></i>
+            <i className="uil uil-graduation-cap qualification-icon"></i>
             Education
           </div>
           <div
-            className='qualification-button'
-            style={{ color: `${toggle === 2 ? 'var(--first-color)' : ''}` }}
+            className="qualification-button"
+            style={{ color: `${toggle === 2 ? "var(--first-color)" : ""}` }}
             onClick={() => setToggle(2)}
           >
-            <i className='uil uil-briefcase-alt qualification-icon'></i>
+            <i className="uil uil-briefcase-alt qualification-icon"></i>
             Work
           </div>
         </div>
 
-        <div className='qualification-section'>
+        <div className="qualification-section">
           {/* content 1 */}
           <div
             className={`qualification-content ${
-              toggle === 1 ? 'active' : 'inactive'
+              toggle === 1 ? "active" : "inactive"
             }`}
           >
             {/* data 1 */}
-            <div className='qualification-data'>
+            <div className="qualification-data">
               <div>
-                <h3 className='qualification-title'>Computer Enginner</h3>
-                <span className='qualification-subtitle'>
-                  Peru - University
-                </span>
-                <div className='qualification-calendar'>
-                  <i className='uil uil-calendar-alt'></i> 2009 - 2004
+                <h3 className="qualification-title">Computer Enginner</h3>
+                <a href="https://www.ifpb.edu.br/">
+                  <span className="qualification-subtitle">
+                    IFPB - University
+                  </span>
+                </a>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2019 - 2024
                 </div>
               </div>
 
               <div>
-                <span className='qualification-rounder'></span>
-                <span className='qualification-line'></span>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
               </div>
             </div>
 
             {/* data 2 */}
-            <div className='qualification-data'>
+            <div className="qualification-data">
               <div></div>
 
               <div>
-                <span className='qualification-rounder'></span>
-                <span className='qualification-line'></span>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
               </div>
 
               <div>
-                <h3 className='qualification-title'>Web Design</h3>
-                <span className='qualification-subtitle'>
-                  Spain - Institute
+                <h3 className="qualification-title">Web Design</h3>
+                <span className="qualification-subtitle">
+                  IFPB ESPERAÇA PB - Institute
                 </span>
-                <div className='qualification-calendar'>
-                  <i className='uil uil-calendar-alt'></i> 2009 - 2004
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2018 - 2020
                 </div>
               </div>
             </div>
 
             {/* data 3 */}
-            <div className='qualification-data'>
+            <div className="qualification-data">
               <div>
-                <h3 className='qualification-title'>Web Development</h3>
-                <span className='qualification-subtitle'>
-                  Youtube - institute
+                <h3 className="qualification-title">Web Development</h3>
+                <span className="qualification-subtitle">
+                  UDEMY - institute
                 </span>
-                <div className='qualification-calendar'>
-                  <i className='uil uil-calendar-alt'></i> 2017 - 2019
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2015 - 2017
                 </div>
               </div>
 
               <div>
-                <span className='qualification-rounder'></span>
-                <span className='qualification-line'></span>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
               </div>
             </div>
 
             {/* data 4 */}
-            <div className='qualification-data'>
+            <div className="qualification-data">
               <div></div>
 
               <div>
-                <span className='qualification-rounder'></span>
+                <span className="qualification-rounder"></span>
                 {/* <span className='qualification-line'></span> */}
               </div>
 
               <div>
-                <h3 className='qualification-title'>Master in Ui/Ux</h3>
-                <span className='qualification-subtitle'>
-                  Lombok - Institute
+                <h3 className="qualification-title">DELPHI </h3>
+                <span className="qualification-subtitle">
+                  REDENTORISTA CAMPINA GRANDE -PB - Institute
                 </span>
-                <div className='qualification-calendar'>
-                  <i className='uil uil-calendar-alt'></i> 2022 - 2024
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2013 - 2015
                 </div>
               </div>
             </div>
@@ -117,61 +119,61 @@ const Qualification = () => {
           {/* content 2 */}
           <div
             className={`qualification-content ${
-              toggle === 2 ? 'active' : 'inactive'
+              toggle === 2 ? "active" : "inactive"
             }`}
           >
             {/* data 1 */}
-            <div className='qualification-data'>
+            <div className="qualification-data">
               <div>
-                <h3 className='qualification-title'>Software Enginner</h3>
-                <span className='qualification-subtitle'>
+                <h3 className="qualification-title">Software Enginner</h3>
+                <span className="qualification-subtitle">
                   Apple - University
                 </span>
-                <div className='qualification-calendar'>
-                  <i className='uil uil-calendar-alt'></i> 2009 - 2004
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2009 - 2004
                 </div>
               </div>
 
               <div>
-                <span className='qualification-rounder'></span>
-                <span className='qualification-line'></span>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
               </div>
             </div>
 
             {/* data 2 */}
-            <div className='qualification-data'>
+            <div className="qualification-data">
               <div></div>
 
               <div>
-                <span className='qualification-rounder'></span>
-                <span className='qualification-line'></span>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
               </div>
 
               <div>
-                <h3 className='qualification-title'>Frontend Developer</h3>
-                <span className='qualification-subtitle'>
+                <h3 className="qualification-title">Frontend Developer</h3>
+                <span className="qualification-subtitle">
                   Spain - Institute
                 </span>
-                <div className='qualification-calendar'>
-                  <i className='uil uil-calendar-alt'></i> 2009 - 2004
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2009 - 2004
                 </div>
               </div>
             </div>
 
             {/* data 3 */}
-            <div className='qualification-data'>
+            <div className="qualification-data">
               <div>
-                <h3 className='qualification-title'>Ui Designer</h3>
-                <span className='qualification-subtitle'>
+                <h3 className="qualification-title">Ui Designer</h3>
+                <span className="qualification-subtitle">
                   Figma - institute
                 </span>
-                <div className='qualification-calendar'>
-                  <i className='uil uil-calendar-alt'></i> 2017 - 2019
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2017 - 2019
                 </div>
               </div>
 
               <div>
-                <span className='qualification-rounder'></span>
+                <span className="qualification-rounder"></span>
                 {/* <span className='qualification-line'></span> */}
               </div>
             </div>
